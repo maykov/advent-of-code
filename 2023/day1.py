e8e6e6ca5baf6e99a3a0bdbf1e15fb095586795a
Alexey Maykov <alexeymaykov@Alexeys-MacBook-Air.local>
2023-12-11 18:30:20 -0800

    Day1 - done

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,16 +1,34 @@
 file_path = "./day1test.txt"
 file_path = "./day1data.txt"
 
+digits = [
+    'zero','one','two','three','four','five','six','seven','eight','nine'
+]
+
 with open(file_path, "r") as file:
-    sum=0
+    sum = 0
     for line in file:
         first_digit = None
         last_digit = None
-        for ch in line:
+        i=0
+        while i < len(line):
+            ch = line[i]
+            num = None
             if ch.isdigit():
+                num = int(ch)
+            else:
+                for digit in digits:
+                    if line[i:].startswith(digit):
+                        num = digits.index(digit)
+                        
+            if num is not None:
                 if first_digit is None:
-                    first_digit = ch
-                last_digit = ch
-        sum += (ord(first_digit)-ord('0')) *10+ (ord(last_digit)-ord('0'))
+                    first_digit = num
+                last_digit = num
+            i += 1
+        checksum = first_digit * 10 + last_digit
+        print(checksum)
+        sum += checksum
     print(sum)
+            
 
